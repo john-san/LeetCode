6d56cab3bdd3b56768910ece18acda7c5e0bc639
John Nguyen <npjohn92@gmail.com>
2023-04-23 05:02:31 -0700

    start recursive solution for 206

--- 206. Reverse Linked List.ts
+++ 206. Reverse Linked List.ts
@@ -1,36 +1,49 @@
 // Definition for singly-linked list.
 class ListNode {
-	val: number;
-	next: ListNode | null;
+	val: number
+	next: ListNode | null
 	constructor(val?: number, next?: ListNode | null) {
-		this.val = val === undefined ? 0 : val;
-		this.next = next === undefined ? null : next;
+		this.val = val === undefined ? 0 : val
+		this.next = next === undefined ? null : next
 	}
 }
 
+// function reverseList(head: ListNode | null): ListNode | null {
+// 	// iterative solution
+// 	let prev: ListNode | null = null;
+// 	let curr = head;
+// 	while (curr) {
+// 		const next = curr.next;
+// 		curr.next = prev;
+// 		prev = curr;
+// 		curr = next;
+// 	}
+// 	return prev;
+
+// }
+
+//recursive solution
 function reverseList(head: ListNode | null): ListNode | null {
-	let prev: ListNode | null = null;
-	let curr = head;
-	while (curr) {
-		const next = curr.next;
-		curr.next = prev;
-		prev = curr;
-		curr = next;
-	}
-	return prev;
+	if (!head || !head.next) return head
+
+	const newHead = reverseList(head.next)
+	head.next.next = head
+	head.next = null
+
+	return newHead
 }
 
 // test
 const head = new ListNode(
 	1,
 	new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
-);
-console.log(reverseList(head));
+)
+console.log(reverseList(head))
 
 // test 2
-const head2 = new ListNode(1, new ListNode(2));
-console.log(reverseList(head2));
+const head2 = new ListNode(1, new ListNode(2))
+console.log(reverseList(head2))
 
 // test 3
-const head3 = null;
-console.log(reverseList(head3));
+const head3 = null
+console.log(reverseList(head3))
