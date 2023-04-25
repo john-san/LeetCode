41be183a9596732bb7b73d27399d2a5747400794
John Nguyen <npjohn92@gmail.com>
2023-04-25 04:46:55 -0700

    add 2nd sol for 876

---
+++ 876. Middle of The Linked List-2nd.ts
@@ -0,0 +1,44 @@
+// Definition for singly-linked list.
+class ListNode {
+	val: number
+	next: ListNode | null
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = val === undefined ? 0 : val
+		this.next = next === undefined ? null : next
+	}
+}
+
+function middleNode(head: ListNode | null): ListNode | null {
+	if (!head) return null
+	if (!head.next) return head
+	let current: ListNode | null = head
+	let next: ListNode | null | undefined = head.next
+	while (next) {
+		current = current!.next
+		next = next?.next?.next
+	}
+
+	return current
+}
+
+console.log(
+	middleNode(
+		new ListNode(
+			1,
+			new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
+		)
+	)
+) // 3
+console.log(
+	middleNode(
+		new ListNode(
+			1,
+			new ListNode(
+				2,
+				new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))
+			)
+		)
+	)
+) // 4
+console.log(middleNode(new ListNode(1))) // 1
+console.log(middleNode(new ListNode(1, new ListNode(2)))) // 2
