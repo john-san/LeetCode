94b7268c8fb209f5abb9fc7e65672234a8140550
John Nguyen <npjohn92@gmail.com>
2023-04-27 05:11:16 -0700

    add space o1 solution for 142

--- 142. Linked List Cycle II.ts
+++ 142. Linked List Cycle II.ts
@@ -8,4 +8,43 @@ class ListNode {
 	}
 }
 
-function detectCycle(head: ListNode | null): ListNode | null {}
+// TC: O(n), SC: O(n)
+// function detectCycle(head: ListNode | null): ListNode | null {
+// 	// create a set to store visited nodes
+// 	const visited = new Set()
+// 	let curr = head
+
+// 	while (curr) {
+// 		if (visited.has(curr)) {
+// 			return curr
+// 		}
+// 		visited.add(curr)
+// 		curr = curr.next
+// 	}
+// 	return null
+// }
+
+// TC: O(n), SC: O(1)
+function detectCycle(head: ListNode | null): ListNode | null {
+	let slow = head
+	let fast = head
+
+	while (fast && fast.next) {
+		slow = slow!.next
+		fast = fast.next.next
+
+		// if there is a cycle, it means slow and fast will meet at some point
+		if (slow === fast) {
+			// reset slow to head and move both slow and fast by 1 step
+			slow = head
+			while (slow !== fast) {
+				slow = slow!.next
+				fast = fast!.next
+			}
+			// return the node where slow and fast meet for the second time
+			return slow
+		}
+	}
+	// if there is no cycle, then fast will reach null
+	return null
+}
