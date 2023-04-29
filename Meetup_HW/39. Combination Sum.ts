71f6108450f2086ab3443c39d58aea164f141f8f
John Nguyen <npjohn92@gmail.com>
2023-04-29 05:03:39 -0700

    add faster solution for 39

--- Meetup_HW/39. Combination Sum.ts
+++ Meetup_HW/39. Combination Sum.ts
@@ -1,4 +1,6 @@
 // TC: O(number of candidates^max depth), SC: O(target value / min value in candidates array)
+// beats 54.50% in terms of runtime, 46.2% in terms of memory
+/*
 function combinationSum(candidates: number[], target: number): number[][] {
 	const result: number[][] = []
 
@@ -24,6 +26,45 @@ function combinationSum(candidates: number[], target: number): number[][] {
 		dfs(i + 1, current, total)
 	}
 	dfs(0, [], 0)
+
+	return result
+}
+*/
+
+// beats 71.9% in terms of runtime, 44.55% in terms of memory
+function combinationSum(candidates: number[], target: number): number[][] {
+	const result: number[][] = []
+
+	/*
+	helper dfs
+	@param path current dfs path of numbers
+	@param sum current sum of numbers
+	@param last last index drawn
+	*/
+	function dfs(path: number[], sum: number, last: number) {
+		// if sum is equal to target, we found a valid combo
+		if (sum == target) {
+			result.push(path)
+			return
+		}
+
+		// if sum is greater than target, return to prevent further dfs calls
+		if (sum > target) return
+
+		// iterate through candidates array, starting at last index drawn
+		for (let i = last; i < candidates.length; i++) {
+			// if sum + current candidate is less than target, we can add it to path
+			if (sum + candidates[i] <= target) {
+				// push a deep copy of path since it will be modified in other dfs calls
+				dfs([...path, candidates[i]], sum + candidates[i], i)
+			}
+		}
+	}
+
+	// sort candidates array in ascending order because we want to draw candidates in ascending order
+	candidates.sort((a, b) => a - b)
+
+	dfs([], 0, 0)
 
 	return result
 }
